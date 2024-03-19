7005a536193bc31ddd488123f835d79dcef9b14f
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-19 22:26:22 +0000

    working on square results

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  erb(:square_results)
 end
