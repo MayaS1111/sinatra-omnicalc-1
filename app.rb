a79854f77e442bf18be40fe7586513e133a5c711
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-19 23:23:31 +0000

    almost done with payment form

--- app.rb
+++ app.rb
@@ -12,3 +12,27 @@ end
 get("/square/results") do
   erb(:square_results)
 end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  erb(:random_results)
+end
