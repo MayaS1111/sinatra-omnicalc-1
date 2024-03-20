c68b0c3e2145bcec13e401da8d2394f3c2871356
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-03-20 00:20:29 +0000

    payment complete

--- app.rb
+++ app.rb
@@ -26,6 +26,19 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
+  @apr = params.fetch("users_number_apr").to_i.to_fs(:percentage, { :precision => 4 })
+  @years = params.fetch("users_number_years").to_i
+  @principal = params.fetch("users_number_principal").to_i.to_fs(:currency)
+
+  r = params.fetch("users_number_apr").to_f / 1200
+  n = params.fetch("users_number_years").to_i * 12
+  pv = params.fetch("users_number_principal").to_i
+
+  numerator = r*(pv)
+  denominator = 1 - (1 + r)**-n
+
+  @payment = (numerator / denominator).to_fs(:currency)
+
   erb(:payment_results)
 end
 
